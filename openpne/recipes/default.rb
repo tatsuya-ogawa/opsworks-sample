9139c8a7e56424014c159f71cbe2b7c88a31c3ff
jmas-ogawa <jmas.tatsuya.ogawa@gmail.com>
2015-03-24 14:18:29 +0900

    add setup recipe.

--- openpne/recipes/default.rb
+++ openpne/recipes/default.rb
@@ -7,8 +7,19 @@
 # All rights reserved - Do Not Redistribute
 #
 
-openpnedir=node[:deploy][:openpne][:deploy_to]
+openpnedir=node[:deploy][:openpne][:deploy_to]+'/current'
+
+%w[OpenPNE.yml ProjectConfiguration.class.php].each do |file|
+	execute 'move config' do
+		code <<-EOS
+		if[ -f #{openpnedir}/config/#{file}.sample]; then
+			mv #{openpnedir}/config/#{file}.sample #{openpnedir}/config/#{file}
+		fi
+EOS
+	end
+end
 
 execute 'openpne setup' do
-	
+	code <<-EOS
+EOS
 end
