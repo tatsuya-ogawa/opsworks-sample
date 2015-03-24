a47bb83be95e05d9d1438ff5dc4fc7f5913ad7f4
jmas-ogawa <jmas.tatsuya.ogawa@gmail.com>
2015-03-24 22:05:48 +0900

    add template.

--- openpne/recipes/default.rb
+++ openpne/recipes/default.rb
@@ -13,18 +13,23 @@ dbuser = node[:deploy][:openpne][:database][:username]
 dbpass = node[:deploy][:openpne][:database][:password]
 dbhost = node[:deploy][:openpne][:database][:host]
 dbname = node[:deploy][:openpne][:database][:database]
-openpne_admin_email = node[:deploy][:openpne][:admin_email] 
-openpne_url = node[:deploy][:openpne][:site_url] 
+mail_domain = node[:deploy][:openpne][:mail_domain] || node[:domain]
+basee_url = node[:deploy][:openpne][:base_url] || "https://#{node[:fqdn]}"
 
 %w[OpenPNE.yml ProjectConfiguration.class.php].each do |file|
-	execute 'move config' do
-		command <<-EOS
-if [ -f #{openpnedir}/config/#{file}.sample ] ; then
-mv #{openpnedir}/config/#{file}.sample #{openpnedir}/config/#{file}
-fi
-EOS
+	#	execute 'move config' do
+	#		command <<-EOS
+	#if [ -f #{openpnedir}/config/#{file}.sample ] ; then
+	#mv #{openpnedir}/config/#{file}.sample #{openpnedir}/config/#{file}
+	#fi
+	#EOS
+	#	end
+	template "#{openpnedir}/config/#{file}" do
+		source "#{file}.erb"	
+		mode '0644'
 	end
 end
+
 
 execute 'openpne setup' do
 	command <<-EOS
