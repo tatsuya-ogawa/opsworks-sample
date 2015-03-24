c08967c66487d7f590814b220641eedb21886d5c
jmas-ogawa <jmas.tatsuya.ogawa@gmail.com>
2015-03-24 13:36:34 +0900

    first commit

---
+++ hello/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: hello
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+file "/home/ec2-user/hello.txt" do
+	action :create
+end
---
+++ openpne/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: openpne
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+openpnedir=node[:deploy][:openpne][:deploy_to]
+
+execute 'openpne setup' do
+	
+end
